abbcd96acb9a042e59deeca267dcbd004fe01546
malfunktion <mentok@protonmail.com>
2026-08-01 03:13:02 -0400

    Create homeLayout.config.ts

---
+++ src/lib/homeLayout.config.ts
@@ -0,0 +1,7 @@
+import type { HomeLayoutItem } from './types';
+
+export const homeLayout: HomeLayoutItem[] = [
+  { id: 'hero', span: 'md:col-span-2 lg:col-span-1' },
+  { id: 'wire', span: 'md:col-span-2 lg:col-span-1' },
+  { id: 'scout', span: 'md:col-span-2 lg:col-span-1' },
+];
