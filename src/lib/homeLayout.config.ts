b65484d9b3b20f9c0a088f7245c7af3b56a93639
malfunktion <mentok@protonmail.com>
2026-08-01 05:58:29 -0400

    Refactor homeLayout configuration for clarity

--- src/lib/homeLayout.config.ts
+++ src/lib/homeLayout.config.ts
@@ -1,7 +1,7 @@
 import type { HomeLayoutItem } from './types';
 
 export const homeLayout: HomeLayoutItem[] = [
-  { id: 'hero', span: 'md:col-span-2 lg:col-span-1' }, // Takes full width of the 2-col grid
-  { id: 'wire', span: 'md:col-span-1 lg:col-span-1' }, // Takes 1 column (half width)
-  { id: 'scout', span: 'md:col-span-1 lg:col-span-1' }, // Takes 1 column (half width)
+  { id: 'hero', span: 'md:col-span-2 lg:col-span-1' },
+  { id: 'wire', span: 'md:col-span-1 lg:col-span-1' },
+  { id: 'scout', span: 'md:col-span-1 lg:col-span-1' },
 ];
