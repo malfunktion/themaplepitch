321e612429d10280504b8ffbb515e8905aab9afc
malfunktion <mentok@protonmail.com>
2026-08-02 02:39:51 -0400

    Refactor home layout item spans for responsiveness
    Updated layout spans for home layout items to improve responsiveness.

--- src/lib/homeLayout.config.ts
+++ src/lib/homeLayout.config.ts
@@ -1,10 +1,10 @@
 import type { HomeLayoutItem } from './types';
 
 export const homeLayout: HomeLayoutItem[] = [
-  { id: 'hero', span: 'md:col-span-1 lg:col-span-1' },
-  { id: 'wire', span: 'md:col-span-1 lg:col-span-1' },
-  { id: 'scout', span: 'md:col-span-2 lg:col-span-1' },
-  { id: 'player-database', span: 'md:col-span-2 lg:col-span-3' },
-  { id: 'stats-dashboard', span: 'md:col-span-2 lg:col-span-3' },
-  { id: 'legends-gallery', span: 'md:col-span-2 lg:col-span-3' },
+  { id: 'hero', span: 'col-span-2 md:col-span-2 lg:col-span-2' },
+  { id: 'wire', span: 'col-span-2 md:col-span-1 lg:col-span-2' },
+  { id: 'scout', span: 'col-span-2 md:col-span-1 lg:col-span-1' },
+  { id: 'player-database', span: 'col-span-2 md:col-span-2 lg:col-span-4' },
+  { id: 'stats-dashboard', span: 'col-span-2 md:col-span-2 lg:col-span-4' },
+  { id: 'legends-gallery', span: 'col-span-2 md:col-span-2 lg:col-span-4' },
 ];
