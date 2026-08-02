092d22684e0506bbfa9578ae05ba536520fe9fc4
malfunktion <mentok@protonmail.com>
2026-08-02 02:25:53 -0400

    Adjust column spans for hero and scout items

--- src/lib/homeLayout.config.ts
+++ src/lib/homeLayout.config.ts
@@ -1,9 +1,9 @@
 import type { HomeLayoutItem } from './types';
 
 export const homeLayout: HomeLayoutItem[] = [
-  { id: 'hero', span: 'md:col-span-2 lg:col-span-1' },
+  { id: 'hero', span: 'md:col-span-1 lg:col-span-1' },
   { id: 'wire', span: 'md:col-span-1 lg:col-span-1' },
-  { id: 'scout', span: 'md:col-span-1 lg:col-span-1' },
+  { id: 'scout', span: 'md:col-span-2 lg:col-span-1' },
   { id: 'player-database', span: 'md:col-span-2 lg:col-span-3' },
   { id: 'stats-dashboard', span: 'md:col-span-2 lg:col-span-3' },
   { id: 'legends-gallery', span: 'md:col-span-2 lg:col-span-3' },
